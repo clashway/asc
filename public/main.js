70308b28a48ea36d94bd0babc1b872498dd029b1
Cory Lashway <cory.lashway@gmail.com>
2013-07-21 03:21:13 -0400

    added voting

--- public/main.js
+++ public/main.js
@@ -12,8 +12,13 @@ socket.on('story list', function(data) {
 
 $(document).ready(function() {
     var frm = $('#story-form');
-    frm.on( "submit", function(event) {
+    $('#story-form').submit(function(event) {
         event.preventDefault();
-        socket.emit('new story', { story : frm.serialize() });
+        socket.emit('new story', { story : $('#story-form').serialize() });
+    });
+
+    $("#story-list a").live('click', function(event) {
+        event.preventDefault();
+        socket.emit('vote', { story_title : $(this).attr('rel') });
     });
 });
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var http    = require('http'),
 var port = 8111;
 
 // Initialize the db
-var dburl = 'localhost/mongoapp';
-var collections = ['backlog'];
+var dburl = 'localhost/asc';
+var collections = ['stories'];
 var db = require('mongojs').connect(dburl, collections);
 
 // Initialize express
@@ -20,37 +20,60 @@ var socket = io.listen(http.createServer(app).listen(port));
 console.log('Connected at: 127.0.0.1:' + port);
 
 // Define the Story object
-function story(description, tshirt) {
+function story(description, tshirt, vote) {
     this.description = description;
     this.tshirt = tshirt;
+    this.vote = vote;
 }
+db.stories.ensureIndex( { "description" : 1 }, {unique : true} );
 
 socket.sockets.on('connection', function(client) {
     // On connection show the user all the current stories
-    db.backlog.find('', function(err, stories) {
-        var storyTitles = '';
-        stories.forEach( function(story) {
-            storyTitles += '<li>' + story.description + '</li>';
-        });
-        client.emit('story list', { stories: storyTitles });
+    getAllStories(function(stories) {
+        client.emit('story list', { stories: stories });
     });
 
     client.on('new story', function(data) {
         // Submit the new story to the db
         var parsed = qs.parse(data.story);
-        var new_story = new story(parsed.description, parsed.tshirt);
-        db.backlog.save(new_story, function (err, success) {
-            // Grab the current stories now after the insert
-            db.backlog.find('', function(err, stories) {
-                var storyTitles = '';
-                stories.forEach( function(story) {
-                    storyTitles += '<li>' + story.description + '</li>';
-                });
-                client.emit('story list', { stories: storyTitles });
-                client.broadcast.emit('story list', { stories: storyTitles });
+        var new_story = new story(parsed.description, parsed.tshirt, 0);
+        db.stories.save(new_story, function (err, success) {
+            getAllStories(function(stories) {
+                client.emit('story list', { stories: stories });
+                client.broadcast.emit('story list', { stories: stories });
             });
         });
      });
 
+    client.on('vote', function(data) {
+        var query = {description: data.story_title};
+        db.stories.find(query, function(err, stories) {
+            stories.forEach( function(voted_story) {
+                voted_story.vote++;
+                db.stories.save(voted_story, function (err, success) {
+                    // Grab the current stories now after the insert
+                    getAllStories(function(stories) {
+                        client.emit('story list', { stories: stories });
+                        client.broadcast.emit('story list', { stories: stories });
+                    });
+                });
+            });
+        });
+    });
 });
 
+function getAllStories(successCallback) {
+    db.stories.find().sort({vote : -1}, function(err, stories) {
+        if (err || !stories) {
+            console.log("No stories found");
+        } else {
+            var storyTitles = '';
+            stories.forEach( function(story) {
+                storyTitles += '<li>';
+                storyTitles += story.description + ' &nbsp <a rel="' + story.description + '" href="#">Vote</a>';
+                storyTitles += '</li>';
+            });
+            successCallback(storyTitles);
+        }
+    });
+};
